cdda4f493be07f998f756e14a0a290ed418408e1
WestleyR <westleyr@nym.hush.com>
2021-01-09 18:14:03 -0800

    Fixed packaging code

--- package.sh
+++ package.sh
@@ -2,13 +2,13 @@
 # Created by: WestleyR
 # Email: westleyr@nym.hush.com
 # Url: https://github.com/WestleyR/srm
-# Last modified date: 2020-08-18
+# Last modified date: 2021-01-09
 #
 # This file is licensed under the terms of
 #
 # The Clear BSD License
 #
-# Copyright (c) 2020 WestleyR
+# Copyright (c) 2020-2021 WestleyR
 # All rights reserved.
 #
 # This software is licensed under a Clear BSD License.
@@ -29,9 +29,9 @@ mkdir -p macos/srm/${TARGET_VERSION}/bin
 mkdir -p armv6l/srm/${TARGET_VERSION}/bin
 
 # Compile for a couple common os/arch
-GOOS=linux GOARCH=amd64 $GO_CMD build $GO_FLAGS -o srm_linux_amd64 ../
-GOOS=darwin GOARCH=amd64 $GO_CMD build $GO_FLAGS -o srm_darwin_amd64 ../
-GOOS=linux GOARCH=arm GOARM=5 $GO_CMD build $GO_FLAGS -o srm_linux_arm ../
+GOOS=linux GOARCH=amd64 $GO_CMD build $GO_FLAGS -o srm_linux_amd64 ../cmd/srm/srm.go
+GOOS=darwin GOARCH=amd64 $GO_CMD build $GO_FLAGS -o srm_darwin_amd64 ../cmd/srm/srm.go
+GOOS=linux GOARCH=arm GOARM=5 $GO_CMD build $GO_FLAGS -o srm_linux_arm ../cmd/srm/srm.go
 
 mv srm_linux_amd64 x86_64_linux/srm/${TARGET_VERSION}/bin/srm
 mv srm_linux_arm armv6l/srm/${TARGET_VERSION}/bin/srm
